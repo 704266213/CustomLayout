da370d7d971d0ec2ae5836203c65a66b7fc37bc3
夏中雷 <704266213@qq.com>
2019-06-26 14:04:24 +0800

    添加滚动和事件拦截

--- app/src/main/java/com/customview/FlowLayoutActivity.kt
+++ app/src/main/java/com/customview/FlowLayoutActivity.kt
@@ -2,11 +2,17 @@ package com.customview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_flow_layout.*
 
 class FlowLayoutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flow_layout)
+
+        clickTest.setOnClickListener {
+            Log.e("XLog","========clickEvent============")
+        }
     }
 }
--- app/src/main/java/com/customview/widget/FlowLayout.kt
+++ app/src/main/java/com/customview/widget/FlowLayout.kt
@@ -2,9 +2,15 @@ package com.customview.widget
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
+import android.view.ViewConfiguration
 import android.view.ViewGroup
+import android.widget.Scroller
+import kotlin.math.abs
 import kotlin.math.max
+
 
 /**
  *
@@ -24,8 +30,15 @@ class FlowLayout(context: Context?) : ViewGroup(context) {
 
     private var lineHeights = mutableListOf<Integer>()
 
+    private lateinit var scroller: Scroller
+    private var scaledDoubleTapSlop = 0
 
-    constructor(context: Context?, attributeSet: AttributeSet) : this(context)
+
+    constructor(context: Context?, attributeSet: AttributeSet) : this(context) {
+        scroller = Scroller(context)
+        val viewConfiguration = ViewConfiguration.get(getContext())
+        scaledDoubleTapSlop = viewConfiguration.scaledDoubleTapSlop
+    }
 
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -138,6 +151,112 @@ class FlowLayout(context: Context?) : ViewGroup(context) {
         constructor(layoutParams: LayoutParams) : super(layoutParams)
 
         constructor(width: Int, height: Int) : super(width, height)
+    }
+
+
+    private var mLastY: Int = 0
+
+    override fun onTouchEvent(ev: MotionEvent): Boolean {
+        val y = ev.y.toInt()
+        when (ev.action) {
+            MotionEvent.ACTION_DOWN -> {
+                if (scroller.isFinished) {
+                    scroller.abortAnimation()
+                }
+                mLastY = y
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val dy = mLastY - y
+                //当前手指位置的坐标
+                val oldScrollY = scrollY
+                //手指偏移后的坐标
+                var preScrollY = oldScrollY + dy
+//                if (preScrollX > (childCount - 1) * height) {
+//                    preScrollX = (childCount - 1) * height
+//                }
+                if (preScrollY < 0) {
+                    preScrollY = 0
+                }
+
+                //scrollTo 是从其实的按下位置开始偏移，每次移动都是相对按下的位置
+//                scrollTo(0, preScrollY)
+                //scrollBy 是从手指当前位置的坐标开始偏移，每次移动都是当前位置的位置
+                // scrollBy(dx,dy)
+
+
+                //开始滑动动画
+                //第一步 finalY 滚动后的位置的坐标（即滚动后当前手指的坐标）滑动完成后的坐标，垂直方向，
+                scroller.startScroll(0, scroller.finalY, 0, dy)
+                //注意，一定要进行invalidate刷新界面，触发computeScroll()方法，因为单纯的startScroll()是属于Scroller的，只是一个辅助类，并不会触发界面的绘制
+                invalidate()
+                mLastY = y
+            }
+        }
+        return true
+    }
+
+    override fun computeScroll() {
+        super.computeScroll()
+        //第二步 判断滚动偏移是否完成
+        if (scroller.computeScrollOffset()) {
+            //第三步 更新滚动偏移， 滑动过程中，根据消耗的时间计算出的当前的滑动偏移距离，垂直方向
+            scrollTo(0, scroller.currY)
+            invalidate()
+        }
+    }
+
+    private var mLastX: Int = 0
+    private var intercepted = false
+
+//    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+//        val currentX = event.x.toInt()
+//        val currentY = event.y.toInt()
+//        when (event.action) {
+//            MotionEvent.ACTION_DOWN, MotionEvent.ACTION_UP -> {
+//                intercepted = false
+//                mLastX = currentX // 用于判断是否拦截的条件
+//                mLastY = currentY // 用于判断是否拦截的条件
+//            }
+//            MotionEvent.ACTION_MOVE -> {
+//                var dx = currentX - mLastX
+//                var dy = currentY - mLastY
+//                intercepted = if (Math.abs(dy) > scaledDoubleTapSlop && Math.abs(dy) > Math.abs(dx)) true else false
+//            }
+//            else -> {
+//            }
+//        }
+//        mLastX = currentX // 用于判断是否拦截的条件
+//        mLastY = currentY // 用于判断是否拦截的条件
+//
+//        return super.dispatchTouchEvent(event)
+//    }
+
+    override fun onInterceptTouchEvent(event: MotionEvent): Boolean {
+        val currentX = event.x.toInt()
+        val currentY = event.y.toInt()
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                intercepted = false
+                mLastX = currentX // 用于判断是否拦截的条件
+                mLastY = currentY // 用于判断是否拦截的条件
+            }
+            MotionEvent.ACTION_MOVE -> {
+                var dx = currentX - mLastX
+                var dy = currentY - mLastY
+                intercepted =  abs(dy) > abs(dx)
+                Log.e("XLog", "====================== dy : ${abs(dy)}")
+                Log.e("XLog", "====================== scaledDoubleTapSlop : ${abs(dy) > scaledDoubleTapSlop}")
+                Log.e("XLog", "======================  : ${abs(dy) > abs(dx)}")
+                Log.e("XLog", "====================== intercepted : $intercepted")
+            }
+            MotionEvent.ACTION_UP -> {
+                intercepted = false
+            }
+        }
+        mLastX = currentX // 用于判断是否拦截的条件
+        mLastY = currentY // 用于判断是否拦截的条件
+
+        return intercepted
     }
 
 }
